e79aa6d7df2ddf90429be2b44c050ca3fd4ebb59
Tomáš Filo <tfilosk@gmail.com>
2020-05-26 21:59:40 +0200

    Default slovencina

--- src/main/java/sk/filo/recipes/config/ConfigProperties.java
+++ src/main/java/sk/filo/recipes/config/ConfigProperties.java
@@ -1,13 +1,17 @@
 package sk.filo.recipes.config;
 
+import java.util.Locale;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /**
  *
@@ -18,7 +22,7 @@ import org.springframework.validation.annotation.Validated;
 @ToString
 @ConfigurationProperties
 @Validated
-public class ConfigProperties {
+public class ConfigProperties implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource() {
@@ -28,5 +32,12 @@ public class ConfigProperties {
         return messageSource;
     }
     
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("sk"));
+        return slr;
+    }
+
 }
 
