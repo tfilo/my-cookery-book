3bf3c4b493218e18f346edeca53e244ab368a0d6
Tomáš Filo <filo@microcomp.sk>
2020-06-03 10:01:30 +0200

    Upravy

--- src/main/java/sk/filo/recipes/component/PDFGenerator.java
+++ src/main/java/sk/filo/recipes/component/PDFGenerator.java
@@ -2,18 +2,22 @@ package sk.filo.recipes.component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 import org.bouncycastle.util.encoders.Base64;
 import org.dom4j.DocumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 import sk.filo.recipes.service.PictureService;
 import sk.filo.recipes.so.PictureSO;
@@ -31,14 +35,22 @@ public class PDFGenerator {
     private static final String MODEL_RECIPE_VIEW_SO = "recipeViewSO";
     
     @Autowired
-    TemplateEngine templateEngine;
-    
-    @Autowired
     MessageSource messageSource;
     
     @Autowired
     PictureService pictureService;
     
+    @Autowired
+    ISpringTemplateEngine templateEngine;
+    
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine);
+        thymeleafViewResolver.setCharacterEncoding("UTF-8");
+        return thymeleafViewResolver;
+    }
+
     public byte[] generate(RecipeViewSO recipeSO) {
         String html = parseThymeleafTemplate(recipeSO);
         
@@ -66,10 +78,11 @@ public class PDFGenerator {
         return outputStream;
     }
     
-    private String parseThymeleafTemplate(RecipeViewSO recipeSO) {
+    private String parseThymeleafTemplate(RecipeViewSO recipeSO) {        
         Context context = new Context();                
         context.setVariable(MODEL_RECIPE_VIEW_SO, recipeSO);
-        
+        context.setLocale(new Locale("sk"));
+
         for (int i = 0; i < recipeSO.getPictures().size(); i++) {
             PictureSO thumbail = pictureService.getThumbnail(recipeSO.getPictures().get(i).getId());
             recipeSO.getPictures().get(i).setBase64(Base64.toBase64String(thumbail.getData()));
@@ -82,7 +95,7 @@ public class PDFGenerator {
                 recipeSO2.getPictures().get(i).setBase64(Base64.toBase64String(thumbail.getData()));
             }
         }
-        
-        return templateEngine.process("pdf/pdf_template", context);
+
+        return templateEngine.process("/pdf/recipe", context);
     }
 }
