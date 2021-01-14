93a78cc1d6832930b2e540fc7b062fb5cb489159
Tomáš Filo <tfilo@users.noreply.github.com>
2021-01-14 19:43:46 +0100

    Update run-postgre-server.sh

--- run-postgre-server.sh
+++ run-postgre-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 5432:5432 -e POSTGRES_DB=recipes -e POSTGRES_PASSWORD=User123 -e POSTGRES_USER=user -v postgre_recipe_data:/var/lib/postgresql/data postgres
+docker run -p 5432:5432 -e POSTGRES_DB=recipes -e POSTGRES_PASSWORD=User123 -e POSTGRES_USER=user -v postgre_recipe_data:/var/lib/postgresql/data postgres:13.1-alpine
